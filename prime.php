78058356a72c9289e0f23ae83e35298ce50434a3
BuddhabhushanDurge <buddhabhushandurge@gmail.com>
2018-04-17 23:11:47 +0530

    Updated finally

--- prime.php
+++ prime.php
@@ -1,4 +1,5 @@
 <?php
+$start = microtime(true);
 $initial = 0 ;
 $fnumber = 2 ;
 while ($initial < 1000 )
@@ -18,4 +19,7 @@ while ($initial < 1000 )
 			}
 	$fnumber=$fnumber+1;
 }
+$time_elapsed_secs = microtime(true) - $start;
+echo"<br>";
+echo "Time taken to execute " .$time_elapsed_secs;
 ?>
