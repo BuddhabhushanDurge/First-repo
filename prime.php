d916bbf72e30fe6f45c328d8010f86298534c6e1
BuddhabhushanDurge <buddhabhushandurge@gmail.com>
2018-04-28 23:36:18 +0530

    Half the execution file

--- prime.php
+++ prime.php
@@ -1,25 +1,44 @@
 <?php
 $start = microtime(true);
-$initial = 0 ;
-$fnumber = 2 ;
-while ($initial < 1000 )
+function loop($num) 
 {
-	$ncount=0;
-		for ( $i=1;$i<=$fnumber;$i++)
-			{
-			if (($fnumber%$i)==0)
-				{
-				$ncount++;
-				}
-			}
-	if ($ncount<3)
-			{
-			echo $fnumber." , ";
-			$initial=$initial+1;
-			}
-	$fnumber=$fnumber+1;
+	
+    $prime = true;
+	
+	for ($i = 2; $i < $num; $i++) 
+	{
+		
+		if (($num % $i)=== 0) 
+		                           
+		{
+            $prime = false; 
+            return $prime;
+         }
+    }
+                    
+    return $prime; 
 }
+?>
+
+<?php
+$initial=1;
+$fnumber=2;
+while ($initial<=1000)   
+{
+    if (loop($fnumber))   
+    {
+        
+        echo $fnumber.",\n";
+         $initial++;
+    }
+    $fnumber++;
+}
+		
+?>	
+<br/>
+<br/>
+<?php 
 $time_elapsed_secs = microtime(true) - $start;
-echo"<br>";
 echo "Time taken to execute " .$time_elapsed_secs;
+	
 ?>
