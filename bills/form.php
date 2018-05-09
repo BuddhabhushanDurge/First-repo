dde251a1d5d88d491bb5901e52c1600a53429670
BuddhabhushanDurge <buddhabhushandurge@gmail.com>
2018-05-09 22:31:21 +0530

    bills task

---
+++ bills/form.php
@@ -0,0 +1,5 @@
+<form method="post" action="action.php" target="_new">
+Total Amount Paid :<input type="text" name="paid"><br/><br/>
+Your Bills : <input type="text" name="bill"><br/><br/>
+<input type="submit" name="ok" value="Send">
+</form>
